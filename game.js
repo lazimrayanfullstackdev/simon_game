06eff100f879593ee025a8a1846402038c188579
lazimrayanfullstackdev <120188655+lazimrayanfullstackdev@users.noreply.github.com>
2023-03-12 23:09:43 +0600

    added user clicked color sequence

--- game.js
+++ game.js
@@ -1,20 +1,57 @@
-var gamePattern = [];
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var gamePattern= [];
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
     return randomNumber;
 }
 
 var randomChosenColour = buttonColours[nextSequence()];
-//console.log("Choosen color is: "+randomChosenColour);
+//console.log("Random Chosen Color is: "+randomChosenColour);
 gamePattern.push(randomChosenColour);
-//console.log("Game Pattern Array is: "+gamePattern);
+$('#'+randomChosenColour).fadeOut(400).fadeIn(400).fadeOut(400).fadeIn(400);
+$('#'+randomChosenColour).click(function(){
+    var tempSound = new Audio(`sounds/${randomChosenColour}.mp3`);
+    tempSound.play();
+});
 
-function flash(){ 
-    $('#'+randomChosenColour).fadeOut(800).fadeIn(800); 
- } 
- $('#'+randomChosenColour).ready(function(){ 
-     setInterval('flash()',200); 
- });
+$(".btn").click(function (){
+    var userChosenColour = this.id;
+    console.log("User Chosen Color is: "+userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
 
-//$('#'+randomChosenColour).fadeOut(900).delay(300).fadeIn(800);
+// $('#'+randomChosenColour).click( function (){
+// setInterval(() => {
+//      $('#'+randomChosenColour).fadeIn();
+//      $('#'+randomChosenColour).fadeOut();
+// }, 100);
+
+// switch (randomChosenColour) {
+//     case green:
+//         var greenaudio = new Audio("sounds/green.mp3");
+//         greenaudio.play();
+//         break;
+
+//     case red:
+//     var redaudio = new Audio("sounds/red.mp3");
+//     redaudio.play();
+//     break;
+
+//     case blue:
+//         var blueaudio = new Audio("sounds/blue.mp3");
+//         blueaudio.play();
+//         break;
+
+//     case yellow:
+//     var yellowaudio = new Audio("sounds/yellow.mp3");
+//     yellowaudio.play();
+//     break;
+
+//     default:
+//     var wrongaudio = new Audio("sounds/wrong.mp3");
+//     wrongaudio.play();        
+//         break;
+// }
+// });
