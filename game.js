46ec1d6cab1ef3a2e2c3e49a120c546df1acb18d
lazimrayanfullstackdev <120188655+lazimrayanfullstackdev@users.noreply.github.com>
2023-03-14 16:12:58 +0600

    User Clicked Button sound and animation added

--- game.js
+++ game.js
@@ -1,57 +1,31 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 var userClickedPattern = [];
-var gamePattern= [];
+var buttonColours = ["red","blue","green","yellow"];
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
+    var randomChosenColour = buttonColours[randomNumber];
+    //console.log("Choosen color is: "+randomChosenColour);
+    gamePattern.push(randomChosenColour);
+    //console.log("Game Pattern Array is: "+gamePattern);
+    $('#'+randomChosenColour).fadeOut(200).fadeIn(200);
+    playSound(randomChosenColour);
 }
-
-var randomChosenColour = buttonColours[nextSequence()];
-//console.log("Random Chosen Color is: "+randomChosenColour);
-gamePattern.push(randomChosenColour);
-$('#'+randomChosenColour).fadeOut(400).fadeIn(400).fadeOut(400).fadeIn(400);
-$('#'+randomChosenColour).click(function(){
-    var tempSound = new Audio(`sounds/${randomChosenColour}.mp3`);
-    tempSound.play();
-});
-
-$(".btn").click(function (){
+nextSequence();
+$("button").click(function (){
     var userChosenColour = this.id;
-    console.log("User Chosen Color is: "+userChosenColour);
+    //console.log(userChosenColour);
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
-
-// $('#'+randomChosenColour).click( function (){
-// setInterval(() => {
-//      $('#'+randomChosenColour).fadeIn();
-//      $('#'+randomChosenColour).fadeOut();
-// }, 100);
-
-// switch (randomChosenColour) {
-//     case green:
-//         var greenaudio = new Audio("sounds/green.mp3");
-//         greenaudio.play();
-//         break;
-
-//     case red:
-//     var redaudio = new Audio("sounds/red.mp3");
-//     redaudio.play();
-//     break;
-
-//     case blue:
-//         var blueaudio = new Audio("sounds/blue.mp3");
-//         blueaudio.play();
-//         break;
-
-//     case yellow:
-//     var yellowaudio = new Audio("sounds/yellow.mp3");
-//     yellowaudio.play();
-//     break;
-
-//     default:
-//     var wrongaudio = new Audio("sounds/wrong.mp3");
-//     wrongaudio.play();        
-//         break;
-// }
-// });
+function playSound(name){
+    var song = new Audio("sounds/"+name+".mp3");
+    song.play();  
+}
+function animatePress(currentColour){
+    $('#'+currentColour).addClass("pressed");
+    setTimeout(function() {
+        $('#'+currentColour).removeClass('pressed');
+      }, 100);
+}
