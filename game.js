29f8d49a7ae688fb45132daeb445534be8852fbe
lazimrayanfullstackdev <120188655+lazimrayanfullstackdev@users.noreply.github.com>
2023-03-21 15:09:38 +0600

    playable game draft

--- game.js
+++ game.js
@@ -9,19 +9,20 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNumber];
     //console.log("Choosen color is: "+randomChosenColour);
     gamePattern.push(randomChosenColour);
-    //console.log("Game Pattern Array is: "+gamePattern);
+    console.log("Game Pattern Array is: "+gamePattern);
     $('#'+randomChosenColour).fadeOut(200).fadeIn(200);
     playSound(randomChosenColour);
-    $("#level-title").html('level '+level);
+    $("#heading2-tag").html('level '+level);
 }
 //nextSequence();
 $(".btn").click(function (){
     var userChosenColour = this.id;
-    console.log(userChosenColour);
+    //console.log(userChosenColour);
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    console.log("User Clicked Pattern is "+userClickedPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
 });
 function playSound(name){
     var song = new Audio("sounds/"+name+".mp3");
@@ -43,6 +44,12 @@ $(document).keypress(function(){
 function checkAnswer(currentLevel){
     if(gamePattern[currentLevel]==userClickedPattern[currentLevel]){
         console.log("Matched - Success");
+        if (userClickedPattern.length==gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+              }, 1000);
+              userClickedPattern = [];
+        }
     }else{
         console.log("Matched - Wrong");
     }
